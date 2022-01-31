5d3ee84e61a6796934e9b611295beba565928ead
frastsytt <frastsytt@gmail.com>
2022-01-31 16:46:42 +0200

    eventEmitter

--- node.js
+++ node.js
@@ -1,17 +1,15 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080); 
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+eventEmitter.emit('scream');
+eventEmitter.emit('scream');
