73cf5feb69d02d5ed3519eef1ce4557a0c9bfc7f
frastsytt <frastsytt@gmail.com>
2022-01-31 16:39:56 +0200

    serve a file from parsed url

--- node.js
+++ node.js
@@ -1,6 +1,17 @@
+var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
-fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-  if (err) throw err;
-  console.log('File Renamed!');
-}); 
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
